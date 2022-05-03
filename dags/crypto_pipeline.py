6c3d6057767be60beec5a69e68d1fd597508d294
zheyanliu99 <zl3119@columbia.edu>
2022-05-02 22:07:00 -0400

    add more comments

--- dags/crypto_pipeline.py
+++ dags/crypto_pipeline.py
@@ -1,31 +1,28 @@
-from jsonschema import ValidationError
+# import airflow packages
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
 from airflow.models import Variable
 
-# connect to aws postgres
+# packages for connection to aws postgres
 import psycopg2
+import psycopg2.extras as extras
+
+
+# other packages
 import pandas as pd
+import datetime
+import requests
 
 dsn = "postgresql://zl3119:1947@w4111.cisxo09blonu.us-east-1.rds.amazonaws.com/proj1part2"
 conn = psycopg2.connect(dsn)
 cur = conn.cursor()
 
-
-import datetime
-import csv
-import requests
-import json
-
-# dd/mm/YY
+# yesterday's date YYYY-mm-dd
 date = (datetime.date.today()- datetime.timedelta(days = 1)).strftime("%Y-%m-%d")
 
 
 # function to insert pandas df to database
-import psycopg2.extras as extras
-from io import StringIO
-
 def execute_values(conn, df, table):
     """
     Using psycopg2.extras.execute_values() to insert the dataframe
@@ -48,7 +45,8 @@ def execute_values(conn, df, table):
     print("execute_values() done")
     cursor.close() 
 
-# get emails in config/variables_config.json
+# get emails list in config/variables_config.json. 
+# Error in this step means that you have not set up variables as in tutorial
 try:
     dag_config = Variable.get('variables_config', deserialize_json=True)
     email_list = dag_config['email_list']
@@ -57,25 +55,19 @@ except:
     email_list = ['yan.test@gmail.com', 'zheyan.test@gmail.com', 'zheyan.test@yahoo.com']
     raise Warning("You didn't set up variables_fig, using default email_list ['yan.test@gmail.com', 'zheyan.test@gmail.com', 'zheyan.test@yahoo.com']")
 
-default_args = {
-    "owner": "airflow",
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "email": "admin@localhost.com",
-    "retries": 1,
-    "retry_delay": datetime.timedelta(minutes=1)
-}
 
+# Task 1: Get crypto
 def get_crypto():
-    print(email_list)
-    # date = '2022-04-28'
+    
     url = 'https://api.polygon.io/v2/aggs/grouped/locale/global/market/crypto/{}?adjusted=true&apiKey=2gEOah0DjDSI9ImMCR_0BL5TSJkvlC4z'.format(date)
     r = requests.get(url)
     data = r.json()
     
+    # the api has limit 5 times/minuate
     if data['status'] == 'ERROR':
         raise(data['error'])
-    # %% transform data
+
+    # transform data
     data['results']
     exchange_symbol = []
     close_price = []
@@ -85,7 +77,6 @@ def get_crypto():
     weighted_average = []
 
     for cr in data['results']:
-        # print(cr)
         exchange_symbol.append(cr['T'])
         close_price.append(cr['c'])
         highest_price.append(cr['h'])
@@ -107,34 +98,47 @@ def get_crypto():
     cr_df = pd.DataFrame(cr_dict)
     cr_df['date'] = date
 
-    # Insert values to df
+    # Insert values to table crytpo_daily, ignore this step if violate primary key constraint
     try:
         execute_values(conn, cr_df, 'crytpo_daily')
     except:
         print('Primary key exists')
     cr_df.to_csv(f'/opt/airflow/dags/files/crypto_{date}.csv', index = False)
         
-
+# Task2: give emails that not recieved any info for the given date
 def check_email(**context):
-    
-    # email_list = ['yan.test@gmail.com', 'zheyan.test@gmail.com']
     cur = conn.cursor()
     cur.execute("SELECT * FROM email_sent WHERE date = %s", (date, ))
     sent_emails = cur.fetchall()
     sent_emails_list = [email[1] for email in sent_emails]
 
     sending_emails = [email for email in email_list if email not in sent_emails_list]
+    # if all email sent, raise error to stop the DAG
     if not sending_emails:
         raise ValueError('All the listed emails recieved crypto info today')
+    # return a str of emails, you can use xcom to get this variable
     return ','.join(sending_emails)
 
-
+# Task4: if email sent successful to some users, update that in the database, so they will not recieve email in the same date
 def update_sent_emails(**context):
     for email in context['ti'].xcom_pull(task_ids='check_emails').split(','):
         cur.execute("INSERT INTO email_sent values(%s, %s)", (date, email))
     conn.commit()
 
-with DAG("crpto_pipline", start_date=datetime.datetime(2021, 1 ,1), 
+# Create a DAG
+
+# default args for the DAG
+default_args = {
+    "owner": "airflow",
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "admin@localhost.com",
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=1)
+}
+
+
+with DAG("crypto_pipeline", start_date=datetime.datetime(2021, 1 ,1), 
     schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
 
     get_crypto = PythonOperator(
@@ -148,6 +152,7 @@ with DAG("crpto_pipline", start_date=datetime.datetime(2021, 1 ,1),
         python_callable=check_email
     )
 
+    # Task3: send email to selected users
     send_emails = EmailOperator(
         task_id='send_emails',
         to="{{ task_instance.xcom_pull(task_ids='check_emails') }}",
